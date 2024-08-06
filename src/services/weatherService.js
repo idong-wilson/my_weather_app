d5807fbc3a2de09f7aab7888ccd1af646ecc993c
[Idong Wilson] <[idongwilson@gmail.com]>
2024-08-06 13:21:02 -0400

    updated doc

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,26 +1,6 @@
 import { DateTime } from "luxon";
 
-let MixString = ['77565', '5b9b7', '76099a', 'ff542', '131d7', '68431f6'];
-let secretOne = "76";
-let secretTwo = "5e7b2";
-let secret = secretTwo.split("").reverse().join("");
-
-let combinedString = MixString.join("");
-
-let reversedCombinedString = combinedString.split("").reverse().join("");
-
-let stepTwo = reversedCombinedString.split(secretOne);
-
-let stepThree = stepTwo.join("");
-
-let key = stepThree.replace(secret, "");
-
-let keyArray = key.split("");
-keyArray.splice(-11, 1); 
-key = keyArray.join("");
-
-
-const API_KEY = `${key}`;
+const API_KEY = `Your API Key`;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 
